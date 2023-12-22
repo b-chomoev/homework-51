36390f2014697ec1fa967fcdf4145c34e610baa8
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2023-12-22 18:53:25 +0600

    Style added to the web page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from 'react';
+import LotteryBall from './components/lotteryBall.tsx';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+    const initialNumbers = [5, 11, 16, 23, 32];
+    const [numbers, setNumbers] = useState<number[]>(initialNumbers);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    const addNewNumbers = () => {
+        const newNumbers = [...Array(5)].map(() => {
+            let randomNum;
+            do {
+                randomNum = Math.floor(Math.random() * 32) + 5;
+            } while (numbers.includes(randomNum));
+            return randomNum;
+        });
 
-export default App
+        newNumbers.sort((a, b) => a - b);
+        setNumbers(newNumbers);
+    };
+
+    return (
+        <div className="app">
+            <button  className="button" onClick={addNewNumbers}>New numbers</button>
+            <div className="lottery-balls">
+                {numbers.map((num, index) => (
+                    <LotteryBall key={index} number={num} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default App;
